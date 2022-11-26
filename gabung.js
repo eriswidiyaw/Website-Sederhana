ac978600a72e110994707c9aa9c886a3c96b7bf2
Erisa Riski Widiya Wati <107670424+eriswidiyaw@users.noreply.github.com>
2022-11-26 18:47:00 +0700

    Create gabung.js

---
+++ gabung.js
@@ -0,0 +1,27 @@
+function goto_home(){
+    location.href='./index.html'
+}
+
+function goto_profil(){
+    location.href='./profilku.html'
+}
+
+function goto_intro(){
+    location.href='./intro.html'
+}
+
+function goto_flowchart(){
+    location.href='./flowchart.html'
+}
+
+function goto_proses(){
+    location.href='./proses.html'
+}
+
+function goto_pengujian(){
+    location.href='./pengujian.html'
+}
+
+function goto_hasil(){
+    location.href='./hasil.html'
+}
